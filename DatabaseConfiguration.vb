e44821c9068ad6bb138b5325461e6f63f258f542
Franc Alvenn Dela Cruz <Administrator@DESKTOP-Q48GDA6>
2023-12-03 23:36:53 +0800

    Admin Changes

--- DatabaseConfiguration.vb
+++ DatabaseConfiguration.vb
@@ -4,17 +4,14 @@
             Return "Data Source=C:/Users/Administrator/source/repos/OOP-Project-Health Symptom Analysis/database/userAuthentication.sqlite;"
         End Get
     End Property
-
     Public ReadOnly Property DataSourceAdminAuthentication As String
         Get
             Return "Data Source=C:/Users/Administrator/source/repos/OOP-Project-Health Symptom Analysis/database/adminAuthentication.sqlite;"
         End Get
     End Property
-
     Public ReadOnly Property DataSourceSystemDatabase As String
         Get
             Return "Data Source=C:/Users/Administrator/source/repos/OOP-Project-Health Symptom Analysis/database/systemDatabase.sqlite;"
         End Get
     End Property
-
 End Module
