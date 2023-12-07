8c6ed00981099077f1be08fbe43180b664b9cc16
christine <christine@LAPTOP-PK4EEMD7>
2023-12-08 07:38:58 +0800

    Update

--- Admin/UserReportFormAll.vb
+++ Admin/UserReportFormAll.vb
@@ -1,23 +1,43 @@
-﻿Imports CrystalDecisions.CrystalReports.Engine
+﻿Imports System.Data.SQLite
+Imports CrystalDecisions.CrystalReports.Engine
 
 Public Class UserReportFormAll
-
+    Public userAuthenticationString As String = DatabaseConfiguration.DataSourceUserAuthentication
+    Dim dataTable As New DataTable()
+    Public Sub SetReport(report As ReportClass)
+        CrystalReportViewer1.ReportSource = report
+    End Sub
     Private Sub ShowReport(report As ReportClass)
-        ' Create an instance of the CrystalReportViewerForm
-        Dim viewerForm As New TestReportForm()
-
         ' Set the report to the CrystalReportViewer in the form
-        viewerForm.SetReport(report)
+        Me.SetReport(report)
 
         ' Show the CrystalReportViewerForm
-        viewerForm.ShowDialog()
+        Me.Show()
     End Sub
+
     Private Sub UserReportFormAll_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim report As ReportClass
         report = New AllUserReport()
-        ' Set the report to the CrystalReportViewer
+        LoadData()
+        report.SetDataSource(dataTable)
+        'ShowReport(report)
         CrystalReportViewer1.ReportSource = report
-        ShowReport(report)
+    End Sub
+
+    Private Sub LoadData()
+
+        If connection.State <> ConnectionState.Open Then
+            connection.Open()
+        End If
+
+        Using Localconnection As New SQLiteConnection(userAuthenticationString)
+
+            Dim query As String = "SELECT ID, Name, Username FROM user_accounts"
+            Using adapter As New SQLiteDataAdapter(query, Localconnection)
+                adapter.Fill(dataTable)
+
+            End Using
+        End Using
 
     End Sub
 End Class
